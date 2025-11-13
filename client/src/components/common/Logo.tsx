6cb7868414664235093696359fb67ac50400048b
Daniel Madubuihe <danielmadubuihe@gmail.com>
2025-11-13 18:59:14 +0100

    FIX: removed unused imports

--- client/src/components/common/Logo.tsx
+++ client/src/components/common/Logo.tsx
@@ -1,10 +1,8 @@
 import { Link } from "react-router-dom";
-import logo from "/logo.png";
 
 const Logo = ({ styles }: { styles: string }) => {
   return (
     <Link to="/">
-      {/* <img src={logo} alt="" className={styles} /> */}
       <p className={styles}>
         <span className="text-red-800">Omni</span>
         <span className="text-zinc-900">Press</span>
